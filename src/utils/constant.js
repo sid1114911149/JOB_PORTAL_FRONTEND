c7586f8feea3c8255bc477e1bc886bbe0f2cff12
siddardha reddy pasham <178729877+sid1114911149@users.noreply.github.com>
2026-03-27 15:00:27 +0530

    third commit

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,7 +4,7 @@
 // export const APPLICATION_API_END_POINT="http://localhost:5000/application";
 // export const COMPANY_API_END_POINT="http://localhost:5000/company";
 // export const BASE_API_END_POINT="https://job-protal-backend-nbt4.onrender.com";
-export const BASE_API_END_POINT="http://localhost:5000";
+export const BASE_API_END_POINT="https://job-protal-backend-vh4d.onrender.com";
 export const USER_API_END_POINT=`${BASE_API_END_POINT}/user`;
 export const JOB_API_END_POINT=`${BASE_API_END_POINT}/job`;
 export const APPLICATION_API_END_POINT=`${BASE_API_END_POINT}/application`;
